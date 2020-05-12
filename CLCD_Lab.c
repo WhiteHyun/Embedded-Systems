325ca19da4fb8669c03a7ca0b66dcb8b54a8c186
WhiteHyun <cx_nf@naver.com>
2020-05-13 03:09:10 +0900

    Fix Error and add debugs

--- CLCD_Lab.c
+++ CLCD_Lab.c
@@ -26,22 +26,21 @@
 
 #define CMD_CURSURMOVE 0x10 //0001 0000 (S/C = 0, R/L = 0)
 
-void write4bits(unsigned char command);                                //4비트 읽어씀
-void sendDataCmd4(unsigned char data);                                 //8비트 데이터를 4비트씩 끊어서 보냄
-void putCmd4(unsigned char cmd);                                       //명령어 입력
-void putChar(char c);                                                  //문자 CLCD에 넣기위한 함수
-void initialize_textlcd(int *inputSet, int *outputSet);                //초기 init 함수
-void waitForEnter(char **expression, char *inputChar, char *inputSet); //입력 받을 때 사용할 함수
-void calculate(bool plusOrMinus, char **start, char **end);            //주어진 식에 대해 계산하는 함수
-void printResult(char *expression, int sum);                           //계산출력용 함수
+void write4bits(unsigned char command);                              //4비트 읽어씀
+void sendDataCmd4(unsigned char data);                               //8비트 데이터를 4비트씩 끊어서 보냄
+void putCmd4(unsigned char cmd);                                     //명령어 입력
+void putChar(char c);                                                //문자 CLCD에 넣기위한 함수
+void initialize_textlcd(int *inputSet, int *outputSet);              //초기 init 함수
+void waitForEnter(char *expression, char *inputChar, int *inputSet); //입력 받을 때 사용할 함수
+int calculate(bool plusOrMinus, char **start, char **end, int sum);  //주어진 식에 대해 계산하는 함수
+void printResult(char *expression);                                  //계산출력용 함수
 
 int main()
 {
+    int i;
     char expression[32] = {
-        NULL,
+        0,
     };
-
-    int sum = 0;
 
     int inputSet[13] = {BTN_PLUS, BTN_MINUS, BTN_EQUAL, BTN_0, BTN_1, BTN_2, BTN_3, BTN_4, BTN_5, BTN_6, BTN_7, BTN_8, BTN_9};
     char inputChar[13] = {'+', '-', '=', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -61,14 +60,28 @@ int main()
     // printf("BTN_PLUS : %d\n", digitalRead(BTN_PLUS));
     // printf("BTN_EQUAL : %d\n", digitalRead(BTN_EQUAL));
 
-    initialize_textlcd();
+    initialize_textlcd(inputSet, outputSet);
     /*Calculate*/
     while (true)
     {
-        for (int i = 0; i < 32; i++)
-            expression[i] = NULL;
-        waitForEnter(&expression, inputChar, inputSet);
-        printResult(expression, sum);
+        for (i = 0; i < 32; i++)
+            expression[i] = 0;
+        while (!(digitalRead(inputSet[3]) || digitalRead(inputSet[4]) || digitalRead(inputSet[5]) ||
+                 digitalRead(inputSet[6]) || digitalRead(inputSet[7]) || digitalRead(inputSet[8]) ||
+                 digitalRead(inputSet[9]) || digitalRead(inputSet[10]) || digitalRead(inputSet[11]) || digitalRead(inputSet[12])))
+        {
+        }
+        while (digitalRead(inputSet[3]) || digitalRead(inputSet[4]) || digitalRead(inputSet[5]) ||
+               digitalRead(inputSet[6]) || digitalRead(inputSet[7]) || digitalRead(inputSet[8]) ||
+               digitalRead(inputSet[9]) || digitalRead(inputSet[10]) || digitalRead(inputSet[11]) || digitalRead(inputSet[12]))
+        {
+        }
+        printf("start waitForEnter Function\n");
+        waitForEnter(expression, inputChar, inputSet);
+        printf("end waitForEnter Function\n");
+        printf("start printResult Function\n");
+        printResult(expression);
+        printf("end printResult Function\n");
     }
     return 0;
 }
@@ -102,26 +115,26 @@ void putCmd4(unsigned char cmd)
 
 void putChar(char c)
 {
-
     digitalWrite(LCD_RS, 1);
-
     sendDataCmd4(c);
 }
 
 void initialize_textlcd(int *inputSet, int *outputSet)
 {
+    int i;
     //CLCD 초기화
-    for (int i = 0; i < 6; i++)
+    for (i = 0; i < 6; i++)
     {
         pinMode(outputSet[i], OUTPUT);
         digitalWrite(outputSet[i], 0);
     }
     delay(35);
-    for (int i = 0; i < 13; i++)
+    for (i = 0; i < 13; i++)
     {
-        pinMode(outputSet[i], INPUT);
-        pullUpDnControl(outputSet[i], PUD_DOWN);
+        pinMode(inputSet[i], INPUT);
+        pullUpDnControl(inputSet[i], PUD_DOWN);
     }
+    delay(35);
     putCmd4(0x28); // 4비트 2줄 Setting (DL = 0, N = 1)
     putCmd4(0x28);
     putCmd4(0x28);
@@ -129,11 +142,15 @@ void initialize_textlcd(int *inputSet, int *outputSet)
     putCmd4(0x02); // 커서 홈
     delay(2);
     putCmd4(0x01); // 표시 클리어
+    putCmd4(0x01);
     delay(2);
+    printf("initialize_done\n");
 }
 
-void waitForEnter(char **expression, char *inputChar, char *inputSet)
+void waitForEnter(char *expression, char *inputChar, int *inputSet)
 {
+    int flag = 0x0000;
+    int i;
     int count = 0;
     int index = 0;
     putCmd4(0x01); // 표시 클리어
@@ -141,73 +158,86 @@ void waitForEnter(char **expression, char *inputChar, char *inputSet)
     //Wait for push
     while (!digitalRead(BTN_EQUAL))
     {
-        for (int i = 0; i < 13; i++)
+        for (i = 0; i < 13; i++)
         {
-            if (digitalRead(inputSet[i]) == HIGH)
+            if (digitalRead(inputSet[i]) == 1)
             {
+                printf("%d = %c\n", i, inputChar[i]);
                 putChar(inputChar[i]);
-                (*expression)[index++] = inputChar[i];
-                putCmd4(CMD_CURSURMOVE | 4);
+                expression[index++] = inputChar[i];
+                //putCmd4(CMD_CURSURMOVE | 4);
                 count++;
-                break;
+                delay(500);
             }
         }
         if (count == 16)
             putCmd4(0xC0); //2행1열로 커서 이동
     }
-    (*expression)[index] = inputChar[2];
+    while (digitalRead(BTN_EQUAL))
+    {
+        delay(35);
+    }
+    expression[index] = inputChar[2];
     putChar(inputChar[2]);
     printf("successfuly Entered\n");
-    printf("expression %s\n", (*expression));
+    printf("expression %s\n", expression);
 }
 
-void calculate(bool plusOrMinus, char **start, char **end)
+int calculate(bool plusOrMinus, char **start, char **end, int sum)
 {
     if (plusOrMinus)
     {
-        sum += strtol(start, &(*end));
+        sum += strtol((*start), &(*end), 10);
     }
     else
     {
-        sum -= strtol(start, &(*end));
+        sum -= strtol((*start), &(*end), 10);
     }
+    return sum;
 }
 
-void printResult(char *expression, int sum)
+void printResult(char *expression)
 {
     int i;
+    int sum = 0;
     char *start = expression;
     bool plusOrMinus = true;
-    char *end; //이전 숫자의 끝 부분을 저장하기 위한 포인터
-    for (i = 0; expression[i] != NULL; i++)
+    char *end = NULL; //이전 숫자의 끝 부분을 저장하기 위한 포인터
+    printf("before expression\n");
+    for (i = 0; expression[i] != 0; i++)
+        printf("%c", expression[i]);
+    printf("\n");
+    for (i = 0; expression[i] != 0; i++)
     {
-        if (strcmp(expression[i], '+') == 0)
+        if (expression[i] == '+')
         {
-            calculate(plusOrMinus, &start, &end);
+            sum = calculate(plusOrMinus, &start, &end, sum);
             plusOrMinus = true;
             start = expression + i + 1;
             continue;
         }
-        else if (strcmp(expression[i], '-') == 0)
+        else if (expression[i] == '-')
         {
-            calculate(plusOrMinus, &start, &end);
+            sum = calculate(plusOrMinus, &start, &end, sum);
             plusOrMinus = false;
             start = expression + i + 1;
             continue;
         }
-        else if (strcmp(expression[i], '=') == 0)
+        else if (expression[i] == '=')
         {
-            calculate(plusOrMinus, &start, &end);
+            printf("=======================\n");
+            printf("start = %d\n", *start);
+            sum = calculate(plusOrMinus, &start, &end, sum);
             break;
         }
     }
-
-    for (i = 0; expression[i] != NULL; i++)
-        expression[i] = NULL;
-    itoa(sum, expression, 10);
-    for (i = 0; expression[i] != NULL; i++)
+    printf("sum = %d\n", sum);
+    for (i = 0; expression[i] != 0; i++)
+        expression[i] = 0;
+    sprintf(expression, "%d", sum);
+    for (i = 0; expression[i] != 0; i++)
     {
+        printf("expression[%d] = %c\n", i, expression[i]);
         putChar(expression[i]);
-        putCmd4(CMD_CURSURMOVE | 4);
     }
 }
